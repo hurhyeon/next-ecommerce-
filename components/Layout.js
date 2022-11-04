72f684281d30f76e063f0f22b7858bef7a538914
허현 <nsksjsk2@hanmail.net>
2022-11-04 10:00:36 +0900

    11-CreateLoginAPI

--- components/Layout.js
+++ components/Layout.js
@@ -2,8 +2,12 @@ import Head from "next/head";
 import Link from "next/link";
 import { React, useContext, useEffect, useState } from "react";
 import { Store } from "../utils/Store";
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { useSession } from 'next-auth/react'
 
 export default function Layout({ title, children }) {
+  const { status, data: session } = useSession()
   const { state } = useContext(Store);
   const { cart } = state;
   const [cartItemsCount, setCartItemsCount] = useState(0);
@@ -17,6 +21,9 @@ export default function Layout({ title, children }) {
         <meta name="description" content="Nextjs" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
+      <ToastContainer position="bottom-center" limit={1} />
+
       <div className="flex min-h-screen flex-col justify-between">
         <header>
           <nav className="flex flex-row h-12 items-center px-4 justify-between shadow-md bg-slate-200">
@@ -37,10 +44,16 @@ export default function Layout({ title, children }) {
                   )}
                 </a>
               </Link>
-              <Link href="/login">
-                <a className="p-2">Login</a>
+              {status === 'loading' ? (
+              'Loading'
+              ) : session?.user ? (
+                session.user.name
+              ) : (
+                <Link href="/login">
+                  <a className="p-2">Login</a>
               </Link>
-            </div>
+            )}
+          </div>
           </nav>
         </header>
         <main className="container m-auto mt-4">{children}</main>
